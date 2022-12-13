33119182e8157c8562c610dee10be3d612a6e0c2
eutales <eu.tsiqueira@gmail.com>
2022-12-13 14:43:45 -0300

    Ultima atualização

--- javabasico/src/com/tales/basico/TiposVariaveis.java
+++ javabasico/src/com/tales/basico/TiposVariaveis.java
@@ -4,8 +4,11 @@ public class TiposVariaveis {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TESTE");
-
+		double salarioMinimo = 2500;
+		float teste = 250.0F;
+		final String CON = "Teste";
+		
+		
 	}
 
 }
